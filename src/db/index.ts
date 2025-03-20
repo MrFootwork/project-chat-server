1eb594ccba21914b714cda0651219eb2cd6f09ec
Pandau <pandau.ting@gmail.com>
2025-03-20 19:08:02 +0100

    setup prisma and test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,1 +1,7 @@
+// const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+// module.exports = prisma;
+export default prisma;
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import prisma from '../db';
 
 const router: Router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.json('All good in here');
+});
+
+router.get('/books', async (req, res) => {
+  try {
+    const books = await prisma.book.findMany();
+    console.log('Found books:', books);
+
+    res.json(books);
+  } catch (error) {
+    console.log(error);
+    res.json('Something went wrong');
+    return;
+  }
 });
 
 module.exports = router;
