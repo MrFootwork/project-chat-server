70928fc6d909aece5316885ebc561aea31a14653
Pandau <pandau.ting@gmail.com>
2025-03-20 19:17:20 +0100

    add POST book route for testing

--- src/app.ts
+++ src/app.ts
@@ -14,27 +14,4 @@ app.use('/api', indexRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
-// TEST
-// const prisma = require('./db/index');
-import prisma from './db/index';
-
-const newBook = {
-  title: 'The Fellowship of the Ring',
-  year: 1954,
-  quantity: 5,
-  genre: ['High fantasy', 'Adventure'],
-  authorName: 'J. R. R. Tolkien',
-};
-
-prisma.book
-  .create({ data: newBook })
-  .then(book => {
-    console.log('Success... a new book was created!!');
-    console.log(book);
-  })
-  .catch(error => {
-    console.log('Something went wrong...');
-    console.log(error);
-  });
-
 module.exports = app;
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -7,6 +7,29 @@ router.get('/', (req, res) => {
   res.json('All good in here');
 });
 
+router.post('/books', async (req, res) => {
+  const { title, author } = req.body;
+  try {
+    const bookData = {
+      title: 'The Fellowship of the Ring',
+      year: 1954,
+      quantity: 5,
+      genre: ['High fantasy', 'Adventure'],
+      authorName: 'J. R. R. Tolkien',
+    };
+
+    const newBook = await prisma.book.create({ data: bookData });
+
+    console.log('New book:', newBook);
+
+    res.json(newBook);
+  } catch (error) {
+    console.log(error);
+    res.json('Something went wrong');
+    return;
+  }
+});
+
 router.get('/books', async (req, res) => {
   try {
     const books = await prisma.book.findMany();
