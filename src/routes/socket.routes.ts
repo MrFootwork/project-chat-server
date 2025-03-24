af8a1c9b4d5a5fff518f7478ebea34560d6c4d90
Pandau <pandau.ting@gmail.com>
2025-03-24 18:54:17 +0100

    allow joining each room by each socket not more than once

--- src/routes/socket.routes.ts
+++ src/routes/socket.routes.ts
@@ -2,18 +2,30 @@ import { Server, Socket } from 'socket.io';
 
 export default async function connectionHandler(socket: Socket, io: Server) {
   const user = socket.handshake.auth.user;
-  console.log(`${socket.id} connected to ${user.name}`);
+  console.log(`${socket.id} connected to "${user.name}" ${user.id}`);
 
   socket.on('join-room', (roomIDs: string[]) => {
-    socket.join(roomIDs);
-    console.log(`${user.name} joined rooms: ${roomIDs}`);
+    // Filter out rooms that the socket is already in
+    const roomsToJoin = roomIDs.filter(roomID => {
+      const room = io.sockets.adapter.rooms.get(roomID);
+      return !room || !room.has(socket.id); // Only join if the socket is not already in the room
+    });
+
+    if (roomsToJoin.length > 0) {
+      socket.join(roomsToJoin);
+      console.log(`${user.name} joined rooms: ${roomsToJoin}`);
+    } else {
+      console.log(`${user.name} is already in the specified rooms.`);
+    }
 
     socket.on('send-message', (roomID, message) => {
       console.log(`${roomID} | ${user.name}: ${message}`);
       if (!roomID) return;
 
-      socket.to(roomID).emit('receive-message', user.name, message);
       // FIXME persist message to database
+      // And send messages after fetched from database
+      // To ensure messages to clients have complete data
+      socket.to(roomID).emit('receive-message', user.name, message);
     });
   });
 
