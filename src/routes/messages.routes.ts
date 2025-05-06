730ab208d5215a8203df943f1fc706d38af791a9
Pandau <pandau.ting@gmail.com>
2025-05-07 01:34:16 +0200

    update

--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -144,7 +144,7 @@ router.post('/notify', async (req: RequestToken, res) => {
         .catch(error => console.error('Error sending notification:', error));
     });
 
-    res.status(500).json({ message: 'Notification sent.' });
+    res.status(200).json({ message: 'Notification sent.' });
   } catch (error) {
     throw error;
   }
